a4c72671ab62bf028dda4d6022a978ba64f1aa0d
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2024-08-30 16:01:31 +0200

    Add metadatas

---
+++ src/metadata.hpp
@@ -0,0 +1,12 @@
+#ifndef METADATA_HPP
+#define METADATA_HPP
+
+
+#define NAME "os-build-utility"
+#define NAME_STR "OS Build Utility"
+#define VERSION "1.0"
+#define AUTHOR "DoorOS Project"
+#define USAGE NAME " [--configure | -c] | [[--build | -b] [--run | -r]]"
+
+
+#endif //METADATA_HPP
