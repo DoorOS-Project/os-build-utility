10ad1e5561a53793b5c4b6a44a055e93577244c9
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2024-09-01 05:53:19 +0200

    Update metadata for v1.0.2

--- src/metadata.hpp
+++ src/metadata.hpp
@@ -4,9 +4,9 @@
 
 #define NAME "os-build-utility"
 #define NAME_STR "OS Build Utility"
-#define VERSION "1.0.1"
+#define VERSION "1.0.2"
 #define AUTHOR "DoorOS Project"
-#define USAGE NAME " [--configure | -c] | [[--build | -b] [--run | -r]]"
+#define USAGE NAME " [--configure | -c] | [[--build | -b] [--run | -r]] [--force | -f]"
 
 
 #endif //METADATA_HPP
